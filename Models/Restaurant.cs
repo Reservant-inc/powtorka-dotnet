6cc6d6a4ab1abadd353327a8bd9557a29b225133
linewelder <redalerm@gmail.com>
2024-01-14 20:35:33 +0100

    Dodano model Restaurant

---
+++ Models/Restaurant.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Powtorka.Models;
+
+public class Restaurant
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required, MaxLength(70)]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    public DateTime AddedAt { get; set; }
+}
