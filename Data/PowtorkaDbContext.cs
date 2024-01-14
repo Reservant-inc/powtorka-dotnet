2bf4486b0fc0d7add39b705f5c3013eeae69e35a
linewelder <redalerm@gmail.com>
2024-01-14 20:48:14 +0100

    Dodano modele do kontekstu

--- Data/PowtorkaDbContext.cs
+++ Data/PowtorkaDbContext.cs
@@ -1,8 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using Powtorka.Models;
 
 namespace Powtorka.Data;
 
 public class PowtorkaDbContext(DbContextOptions<PowtorkaDbContext> options)
     : DbContext(options)
 {
+    public DbSet<Restaurant> Restaurants { get; set; } = null!;
+    public DbSet<MenuItem> MenuItems { get; set; } = null!;
 }
