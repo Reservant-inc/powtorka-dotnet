1e77a7e118d01040d5f07beb3642c9fb44910c41
s24845 <s24845@pjwstk.edu.pl>
2024-01-15 17:02:16 +0100

    Dodano MenuController z pierwszą końcówką

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Powtorka.Data;
+using Powtorka.Models;
+
+namespace Powtorka.Controllers
+{
+
+
+    [ApiController, Route("/api/restaurants")]
+    public class MenuController(PowtorkaDbContext context) : Controller
+    {
+
+        // GET Zwraca wszystkie MenuItem przypisane do danej restauracji.
+        [HttpGet, Route("{restaurantId:int}/menu")]
+        public async Task<IActionResult> GetMenuItem(int restaurantId)
+        {
+            return Ok(await context.MenuItems
+                .Where(m => m.RestaurantId == restaurantId)
+                .ToListAsync()
+            );
+        }
+
+
+
+    }
+}
