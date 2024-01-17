c08210b8a613a674f1319c51e44549b9f251d5c4
Michal-Gi <s24333@pjwstk.edu.pl>
2024-01-17 02:17:37 +0100

    Dodano MenuItemDTO

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Powtorka.Controllers
+{
+    [Route("/api/restaurants/{restaurantId:int}/menu")]
+    [ApiController]
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ DTOs/MenuItemDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Powtorka.DTOs
+{
+    public class MenuItemDTO
+    {
+        public required string Name {  get; set; }
+        public required decimal Price { get; set; }
+    }
+}
