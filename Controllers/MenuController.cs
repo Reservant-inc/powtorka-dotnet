30b5f1bed9128eaae9a058430cf95ddf7404f402
Aleksander Babij <s25347@pjwstk.edu.pl>
2024-01-20 20:01:57 +0100

    dodano MenuController
    dodano końcówkę Get(restaurantId)

---
+++ Controllers/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Powtorka.Data;
+using Powtorka.Models;
+using Powtorka.Models.DTOs;
+
+namespace Powtorka.Controllers
+{
+    [ApiController, Route("/api/restaurants/{restaurantId: int}")]
+    public class MenuController(PowtorkaDbContext _context): Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get(int restaurantId)
+        {
+            var menuItems = await _context.MenuItems
+                .Where(m => m.RestaurantId == restaurantId)
+                .ToListAsync();
+
+            return Ok(menuItems.Select(m => new GetMenuItemDTO
+            {
+                Id = m.Id,
+                Name = m.Name,
+                Price = m.Price,
+                RestaurantId = restaurantId
+            }));
+        }
+    }
+}
